1ee0fdcc488c2c3fd8c54d2f7f63a7dcdcb3daa2
Sh-karan27 <shuklakaran107@gmail.com>
2024-04-21 13:06:39 +0530

    add user aand video model

---
+++ src/models/video.model.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudnary url
+      required: true,
+    },
+    thumb: {
+      type: String, //cloudnary url
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    duration: {
+      type: Number, //cloudnary url
+      required: true,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,15 @@
 const asyncHandler = (requestHandler) => {
-  (req, res) => {
+  (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
 
 export { asyncHandler };
+
+// const asyncHandler = ()=>{}
+// const asyncHandler = ()=>{()=>{}}
+// const asyncHandler = ()=>()=>{}
+// const asyncHandler = ()=>async()=>{}
 
 // const asyncHandler = (fn) => async (req, res, next) => {
 //   try {
