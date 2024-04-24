4202779d4c16e294cadfde35f8390d80ed78cd4d
Sh-karan27 <shuklakaran107@gmail.com>
2024-04-24 11:56:49 +0530

    bug fixed in getCurrentUser method in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -253,7 +253,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (res, req) => {
   return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully");
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
