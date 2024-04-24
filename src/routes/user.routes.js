39798f96ee0010dedb5fd7267264c37a9bc28c43
Sh-karan27 <shuklakaran107@gmail.com>
2024-04-24 17:29:57 +0530

    typo error in routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -47,7 +47,7 @@ router
 
 router
   .route("/update-cover-image")
-  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
