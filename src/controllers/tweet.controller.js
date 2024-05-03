a539c3c019d74553599cf1e819b552687f1a4a6c
Sh-karan27 <shuklakaran107@gmail.com>
2024-05-03 17:54:58 +0530

    added tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Like } from "../models/like.model.js";
 
 const createTweet = asyncHandler(async (req, res) => {
   //TODO: create tweet
@@ -29,6 +30,68 @@ const createTweet = asyncHandler(async (req, res) => {
 
 const getUserTweets = asyncHandler(async (req, res) => {
   // TODO: get user tweets
+  const { userId } = req.params;
+  // console.log(userId);
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(404, "please enter a valid userID");
+  }
+
+  const userTweets = await Tweet.aggregate([
+    {
+      $match: { owner: new mongoose.Types.ObjectId(userId) },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "userDetails",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "tweets",
+        as: "likeDetails",
+        pipeline: [
+          {
+            $project: {
+              likedBy: 1,
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likeDetails",
+        },
+        userDetails: {
+          $first: "$userDetails",
+        },
+      },
+    },
+  ]);
+
+  if (!userTweets) {
+    throw new ApiError(404, "something went wrong while fetching twweets");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, userTweets, "user tweets fetched"));
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -11,7 +11,7 @@ const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/").post(verifyJWT, createTweet);
-router.route("/user/:userId").get(getUserTweets);
+router.route("/user/:userId").get(verifyJWT, getUserTweets);
 router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
 
 export default router;
