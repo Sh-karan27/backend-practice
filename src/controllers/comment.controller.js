0d58d0ff9e346f4584247ebe9dc0bc700545b092
Sh-karan27 <shuklakaran107@gmail.com>
2024-05-15 14:13:28 +0530

    added comment.controller.js/comment.router.js

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,6 +3,7 @@ import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
@@ -23,6 +24,11 @@ const addComment = asyncHandler(async (req, res) => {
     throw new ApiError(404, "Add content for comment");
   }
 
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
   const comment = await Comment.create({
     content,
     video: videoId,
@@ -40,6 +46,49 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(404, "Please enter content");
+  }
+
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(404, "enter valid comment id ");
+  }
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+
+  if (comment?.owner.toString() !== req.user?._id.toString()) {
+    throw new ApiError(
+      400,
+      "You are not are not the owner of comment cant edit"
+    );
+  }
+
+  const updatedComment = await Comment.findByIdAndUpdate(
+    comment?._id,
+    {
+      $set: {
+        content,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedComment) {
+    throw new ApiError(500, "failed to update commnet, try again");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedComment, "comment updated successfully"));
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
