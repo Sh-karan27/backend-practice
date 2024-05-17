eb1a25b9d6300df8dfd449c66fd9fb6e0a9d7878
Sh-karan27 <shuklakaran107@gmail.com>
2024-05-17 13:40:08 +0530

    dashboard.controller.js completed

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 //routes dclaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
@@ -33,5 +34,6 @@ app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
